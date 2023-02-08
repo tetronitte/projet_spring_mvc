6a900f0165d4f771f7e3a9395bd2c76ffa66a7a0
tetronitte <68910823+tetronitte@users.noreply.github.com>
2023-02-08 15:07:54 +0100

    Affiliate entity

--- src/main/java/fr/dawan/projet_spring_mvc/entities/Contact.java
+++ src/main/java/fr/dawan/projet_spring_mvc/entities/Contact.java
@@ -12,6 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Contact {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JoinColumn(name = "user_id")
