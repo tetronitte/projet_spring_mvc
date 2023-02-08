86bfa3923eba2a57baab06982ab28dd0081bc11f
tetronitte <68910823+tetronitte@users.noreply.github.com>
2023-02-08 13:43:51 +0100

    first commit

---
+++ src/main/java/fr/dawan/projet_spring_mvc/ProjetSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.projet_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/dawan/projet_spring_mvc/ProjetSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.projet_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
