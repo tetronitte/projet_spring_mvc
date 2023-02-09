8a73da44984e1dee5e29ff092903dd4e1f112559
Akela <haouamyassine@gmail.com>
2023-02-09 10:41:28 +0100

    formulaire ajout de contact + preparation du post via controller

---
+++ src/main/java/fr/dawan/projet_spring_mvc/services/ContactService.java
@@ -0,0 +1,11 @@
+package fr.dawan.projet_spring_mvc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+    @Autowired
+    private ContactService contactService;
+
+}
