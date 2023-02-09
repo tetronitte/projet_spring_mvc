7b9f7c7e76efceca0a065257b5160005d433fb12
tetronitte <68910823+tetronitte@users.noreply.github.com>
2023-02-09 14:21:58 +0100

    add search method

--- src/main/java/fr/dawan/projet_spring_mvc/repositories/ContactRepository.java
+++ src/main/java/fr/dawan/projet_spring_mvc/repositories/ContactRepository.java
@@ -4,6 +4,10 @@ import fr.dawan.projet_spring_mvc.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "contact", path = "contact")
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    public List<Contact> findByFirstnameContainingOrLastnameContainingOrEmailContainingOrPhoneContaining(String firstname, String lastname, String email, String phone);
 }
