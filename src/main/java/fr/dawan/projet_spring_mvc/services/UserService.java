1063fcbfc7a89cd03c8066b3421d4ec9a4a54128
tetronitte <68910823+tetronitte@users.noreply.github.com>
2023-02-10 09:23:50 +0100

    UserService

--- src/main/java/fr/dawan/projet_spring_mvc/services/UserService.java
+++ src/main/java/fr/dawan/projet_spring_mvc/services/UserService.java
@@ -1,4 +1,26 @@
 package fr.dawan.projet_spring_mvc.services;
 
+import fr.dawan.projet_spring_mvc.dto.UserDTO;
+import fr.dawan.projet_spring_mvc.entities.User;
+import fr.dawan.projet_spring_mvc.repositories.UserRepository;
+import fr.dawan.projet_spring_mvc.tools.PasswordHasher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordHasher passwordHasher;
+
+    public Optional<UserDTO> authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (passwordHasher.matches(password, user.getPassword())) return Optional.of(UserDTO.convertFromEntity(user));
+        return Optional.empty();
+    }
 }
