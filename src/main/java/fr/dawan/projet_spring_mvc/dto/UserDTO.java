b86df065288b0fee9e47ae1ce0375193984fd662
tetronitte <68910823+tetronitte@users.noreply.github.com>
2023-02-10 09:25:30 +0100

    UserDTO class

--- src/main/java/fr/dawan/projet_spring_mvc/dto/UserDTO.java
+++ src/main/java/fr/dawan/projet_spring_mvc/dto/UserDTO.java
@@ -1,4 +1,33 @@
 package fr.dawan.projet_spring_mvc.dto;
 
+import fr.dawan.projet_spring_mvc.entities.Contact;
+import fr.dawan.projet_spring_mvc.entities.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class UserDTO {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String pictures;
+    private String password;
+    private List<Contact> contacts;
+
+    public static UserDTO convertFromEntity(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstname(user.getFirstname());
+        userDTO.setLastname(user.getLastname());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPictures(user.getPictures());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setContacts(user.getContacts());
+        return userDTO;
+    }
 }
